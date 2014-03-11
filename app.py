c0ccba5ff0d247df0b1ce0d4a8de0dbf5cd79d8f
JCKim <shinvee@gmail.com>
2014-03-11 16:39:45 +0900

    훅 형태 제대로 파악

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 from flask import Flask, request, jsonify
 
@@ -6,40 +7,40 @@ app = Flask(__name__)
 url = ('https://spoqa.slack.com/services/hooks/incoming-webhook'
        '?token=EqCiU0T2MCimJLsn30j7JUPF')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = json.loads(request.data)
-    if 'issue_comment' in data:
-        for label in data['issue_comment']['issue']['labels']:
+    if request.headers['X-GitHub-Event'] == 'issue_comment':
+        for label in data['issue']['labels']:
             payload = {
                 "username": "github",
                 "icon_emoji": ":octocat:",
-                "channel":u"#{0}".format(label['name']),
+                "channel": u"#{0}".format(label['name']),
                 "text": u"#{0} @{1}: {2}\n<{2}>".format(
-                    data['issue_comment']['issue']['id'],
-                    data['issue_comment']['comment']['user']['login'],
-                    data['issue_comment']['comment']['body'],
-                    data['issue_comment']['comment']['url']
+                    data['issue']['id'],
+                    data['comment']['user']['login'],
+                    data['comment']['body'],
+                    data['comment']['url']
                 )
             }
-            r=requests.post(url, data=json.dumps(payload))
-    if 'issues' in data:
-        for label in data['issues']['issue']['labels']:
+            r = requests.post(url, data=json.dumps(payload))
+    if request.headers['X-GitHub-Event'] == 'issues':
+        for label in data['issue']['labels']:
             payload = {
                 "username": "github",
-                "icon_emoji": ":octocat:",
-                "channel":u"#{0}".format(label['name']),
+                "con_emoji": ":octocat:",
+                "channel": u"#{0}".format(label['name']),
                 "text": u"#{0} {1} by @{2}\n{3}\n<{4}>".format(
-                    data['issues']['issue']['id'],
-                    data['issues']['issue']['title'],
-                    data['issues']['issue']['user']['login'],
-                    data['issues']['issue']['body'],
-                    data['issues']['issue']['url']
+                    data['issue']['id'],
+                    data['issue']['title'],
+                    data['issue']['user']['login'],
+                    data['issue']['body'],
+                    data['issue']['url']
                 )
             }
-            r=requests.post(url, data=json.dumps(payload))
+            r = requests.post(url, data=json.dumps(payload))
     return jsonify(result='success')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
