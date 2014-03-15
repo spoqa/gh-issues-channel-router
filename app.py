e355b133143bc6c00665159774cb3a87dfbb51fd
Sean H. Jung <sean@spoqa.com>
2014-03-16 00:32:51 +0900

    각 이벤트를 처리하는 메소드를 정의하도록 변경 if 로 이벤트마다 분기를 해서 처리하던것을 각 이벤트별 핸들러를 정의해서 처리할 수 있도록 수정했습니다.

--- app.py
+++ app.py
@@ -8,38 +8,68 @@ url = ('https://spoqa.slack.com/services/hooks/incoming-webhook'
        '?token=EqCiU0T2MCimJLsn30j7JUPF')
 
 
+class GhEventHandler(object):
+
+    _handler_map = {}
+
+    def add_event(self, event):
+        def decorator(handler):
+            if event not in self._handler_map:
+                self._handler_map[event] = handler
+
+            def decorated_function(*args, **kwargs):
+                return handler(*args, **kwargs)
+            return decorated_function
+        return decorator
+
+    def handle(self):
+        event = request.headers['X-GitHub-Event']
+        if event in self._handler_map:
+            data = json.loads(request.data)
+            self._handler_map[event](data)
+
+
+handler = GhEventHandler()
+
+
+@handler.add_event("issue_comment")
+def issue_comment(data):
+    for label in data['issue']['labels']:
+        payload = {
+            "username": "github",
+            "icon_emoji": ":octocat:",
+            "channel": u"#{0}".format(label['name']),
+            "text": u"#{0} @{1}: {2}\n<{3}>".format(
+                data['issue']['number'],
+                data['comment']['user']['login'],
+                data['comment']['body'],
+                data['comment']['html_url']
+            )
+        }
+        requests.post(url, data=json.dumps(payload))
+
+
+@handler.add_event("issues")
+def issues(data):
+    for label in data['issue']['labels']:
+        payload = {
+            "username": "github",
+            "con_emoji": ":octocat:",
+            "channel": u"#{0}".format(label['name']),
+            "text": u"#{0} {1} by @{2}\n{3}\n<{4}>".format(
+                data['issue']['number'],
+                data['issue']['title'],
+                data['issue']['user']['login'],
+                data['issue']['body'],
+                data['issue']['html_url']
+            )
+        }
+        requests.post(url, data=json.dumps(payload))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    data = json.loads(request.data)
-    if request.headers['X-GitHub-Event'] == 'issue_comment':
-        for label in data['issue']['labels']:
-            payload = {
-                "username": "github",
-                "icon_emoji": ":octocat:",
-                "channel": u"#{0}".format(label['name']),
-                "text": u"#{0} @{1}: {2}\n<{3}>".format(
-                    data['issue']['number'],
-                    data['comment']['user']['login'],
-                    data['comment']['body'],
-                    data['comment']['html_url']
-                )
-            }
-            r = requests.post(url, data=json.dumps(payload))
-    if request.headers['X-GitHub-Event'] == 'issues':
-        for label in data['issue']['labels']:
-            payload = {
-                "username": "github",
-                "con_emoji": ":octocat:",
-                "channel": u"#{0}".format(label['name']),
-                "text": u"#{0} {1} by @{2}\n{3}\n<{4}>".format(
-                    data['issue']['number'],
-                    data['issue']['title'],
-                    data['issue']['user']['login'],
-                    data['issue']['body'],
-                    data['issue']['html_url']
-                )
-            }
-            r = requests.post(url, data=json.dumps(payload))
+    handler.handle()
     return jsonify(result='success')
 
 if __name__ == "__main__":
